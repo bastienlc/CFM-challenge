32bc774795899dc5c83cd340b52e1a68445f5c9e
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-03-16 20:56:33 +0100

    Remove diffpool

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from .base import Base
-from .diffpool import DiffPool
 from .gat import GATEncoder
 from .gen import GENEncoder
 from .general import GeneralEncoder
--- src/models/diffpool/__init__.py
+++
@@ -1,1 +0,0 @@
-from .diffpool import DiffPool
