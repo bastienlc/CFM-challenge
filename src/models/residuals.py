8761104269a2d47ccab55351dfb0f2ef8e651c39
bastienlc <78959054+bastienlc@users.noreply.github.com>
2024-03-17 22:24:23 +0100

    Better residuals

--- src/models/residuals.py
+++ src/models/residuals.py
@@ -2,13 +2,13 @@ import os
 import pickle
 
 import numpy as np
+from sklearnex import patch_sklearn
+
+patch_sklearn()
+
 from sklearn.base import ClassifierMixin
-from sklearn.ensemble import (
-    RandomForestClassifier,
-    RandomForestRegressor,
-    StackingRegressor,
-)
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier, StackingRegressor
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.neighbors import KNeighborsRegressor
 
 verbose = 0
@@ -20,16 +20,15 @@ class ResidualModel(ClassifierMixin):
         self.n_jobs = n_jobs
 
         self.base_classifier = RandomForestClassifier(
-            verbose=verbose, n_jobs=self.n_jobs
+            verbose=verbose,
+            n_jobs=self.n_jobs,
+            max_depth=14,
         )
         self.regressors = []
         for _ in range(24):
             self.regressors.append(
                 [
-                    (
-                        "RandomForestRegressor",
-                        RandomForestRegressor(verbose=verbose, n_jobs=self.n_jobs),
-                    ),
+                    ("Ridge", Ridge(n_jobs=self.n_jobs)),
                     ("KNeighborsRegressor", KNeighborsRegressor(n_jobs=self.n_jobs)),
                     ("LinearRegression", LinearRegression(n_jobs=self.n_jobs)),
                 ]
@@ -44,12 +43,12 @@ class ResidualModel(ClassifierMixin):
             )
 
     def fit(self, X, y):
-        one_hot_y = np.eye(np.max(y) + 1)[y]
-
         print("Training base classifier...")
         self.base_classifier.fit(X, y)
+        base_prediction = self.base_classifier.predict_proba(X)
+        print("Base classifier accuracy", (base_prediction.argmax(1) == y).mean())
 
-        y_train_residuals = one_hot_y - self.base_classifier.predict_proba(X)
+        y_train_residuals = np.eye(np.max(y) + 1)[y] - base_prediction
 
         for k in range(24):
             print(f"Training regressor {k}...")
@@ -66,11 +65,13 @@ class ResidualModel(ClassifierMixin):
         return np.argmax(self.predict_proba(X), axis=1)
 
     def save(self, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
         with open(os.path.join(path, "base_classifier.pkl"), "wb") as f:
             pickle.dump(self.base_classifier, f)
 
         for k in range(24):
-            with open(os.path.join(path, f"stacking_regressor_{k}.pkl")) as f:
+            with open(os.path.join(path, f"stacking_regressor_{k}.pkl"), "wb") as f:
                 pickle.dump(self.stacking_regressors[k], f)
 
     def load(self, path):
